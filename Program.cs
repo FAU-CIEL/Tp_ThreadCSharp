77680ed1dcb47899610cd2aa71ec9ab6bbe570a0
Theotime Faure <theotimefaure@free.fr>
2026-01-08 09:04:21 +0100

    Commit #2

--- Program.cs
+++ Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Tp_ThreadCSharp;
+
+class Program
+{
+    static void AfficheA()
+    {
+        while (true)
+        {
+            Console.WriteLine("AfficheA thread : " + Thread.CurrentThread.Name);
+            Thread.Sleep(1000);
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine(Thread.CurrentThread.Name);
+        Console.WriteLine(Thread.CurrentThread.ThreadState);
+
+        Thread.CurrentThread.Name = "Thread 1";
+        Console.WriteLine(Thread.CurrentThread.Name);
+
+        Thread thread1_fonction = new Thread(AfficheA);
+        Thread thread2_fonction = new Thread(AfficheA);
+        Thread thread3_fonction = new Thread(AfficheA);
+
+        thread1_fonction.Name = "thread1_fonction1";
+        thread2_fonction.Name = "thread2_fonction1";
+        thread3_fonction.Name = "thread3_fonction1";
+
+        thread1_fonction.Start();
+        thread2_fonction.Start();
+        thread3_fonction.Start();
+    }
+}
