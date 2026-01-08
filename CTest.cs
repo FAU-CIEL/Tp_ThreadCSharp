314c3531a277db4af5a73e06d4e24eda3a0c82ca
Theotime Faure <theotimefaure@free.fr>
2026-01-08 10:26:59 +0100

    commit #5

--- CTest.cs
+++ CTest.cs
@@ -2,6 +2,20 @@ namespace CTest
 {
     public class CTest
     {
+
+        private int div;
+
+        private int calcul(int val)
+        {
+            div = val;
+            if (div != 0)
+            {
+                Thread.Sleep(1);
+                return (100 / div);
+            }
+            return 0;
+        }
+
         public void AfficheB()
         {
             for (int i = 1; i <= 50; i++)
@@ -15,7 +29,9 @@ namespace CTest
         {
             for (int i = 1; i <= 100; i++)
             {
+                div = 0;
                 Console.WriteLine("AfficheC thread : " + Thread.CurrentThread.Name + " iteration " + i);
+                Console.WriteLine(calcul(i-1));
                 Thread.Sleep(100);
             }
         }
